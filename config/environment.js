ebbea69f6ee7d9be4e62b1d077ce9a98a29ba3fc
freshmanacadamy <freshmanacadamy@gmail.com>
2025-11-25 10:29:35 +0300

    Update environment.js

---
+++ config/environment.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = {
+    BOT_TOKEN: process.env.BOT_TOKEN,
+    CHANNEL_ID: process.env.CHANNEL_ID,
+    ADMIN_IDS: process.env.ADMIN_IDS ? process.env.ADMIN_IDS.split(',').map(Number) : [5747226778],
+    REGISTRATION_FEE: parseInt(process.env.REGISTRATION_FEE) || 500,
+    REFERRAL_REWARD: parseInt(process.env.REFERRAL_REWARD) || 30,
+    MIN_REFERRALS_FOR_WITHDRAW: parseInt(process.env.MIN_REFERRALS_FOR_WITHDRAW) || 4,
+    BOT_USERNAME: process.env.BOT_USERNAME || 'JU1confessionbot'
+};
