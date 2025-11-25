967f1396c992a2f66473d0deb13d65a50dd4f53f
freshmanacadamy <freshmanacadamy@gmail.com>
2025-11-25 19:43:48 +0300

    Update admin.js

--- handlers/admin.js
+++ handlers/admin.js
@@ -29,8 +29,8 @@ const options = {
                 [{ text: '📈 Registration Trends' }, { text: '👤 Add Admin' }],
                 [{ text: '🔧 Maintenance Mode' }, { text: '✉️ Message Student' }],
                  [{ text: '📚 Upload Tutorial' }, { text: '📚 Manage Tutorials' }],
-                [{ text: '📢 Broadcast Message' }, { text: '⚙️ Bot Settings' }]
-                [{ text: '📚 Tutorials' }],
+                [{ text: '📢 Broadcast Message' }, { text: '⚙️ Bot Settings' }],
+                [{ text: '📚 Tutorials' }]
             ],
             resize_keyboard: true
         }
@@ -149,6 +149,7 @@ module.exports = {
     handleAdminDetails,
     handleAdminStats
 };
+
 
 
 
