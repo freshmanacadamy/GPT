7f6ec48bb89eb3293eab8132319f7be4c5ce0a64
freshmanacadamy <freshmanacadamy@gmail.com>
2025-11-26 06:36:40 +0300

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -7,7 +7,7 @@ module.exports = {
     REGISTRATION_FEE: parseInt(process.env.REGISTRATION_FEE) || 500,
     REFERRAL_REWARD: parseInt(process.env.REFERRAL_REWARD) || 30,
     MIN_REFERRALS_FOR_WITHDRAW: parseInt(process.env.MIN_REFERRALS_FOR_WITHDRAW) || 4,
-    BOT_USERNAME: process.env.BOT_USERNAME || 'JU1confessionbot'
-    INVITE_SYSTEM_ENABLED: process.env.INVITE_SYSTEM_ENABLED !== 'false' // Default to true
+    BOT_USERNAME: process.env.BOT_USERNAME || 'freshman_academy_jmubot'
 };
+
 
